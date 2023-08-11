2cb281c856c9a5f540cf12ecaef5e30179b28966
danielacarva <DCarva23@hotmail.com>
2023-08-11 13:36:13 -0300

    instalando bootstrap

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <ul>
+    <li>
+      
+      <h2>Home</h2>
+    </li>
+  </ul>
+
+  <h1 class="titulo">BANCO DE TALENTOS</h1>>
+</template>
+
+<style scoped>
+.titulo{
+  font-size: 34px;
+  font-weight: bold;
+  color: #008B8B;
+  text-align: center;
+}
+
+</style>
